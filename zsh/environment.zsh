b09b2eb4196144b462dbe22c29171ec2088e3ba8
turnspike <hello@turnspike.com>
2021-10-07 12:01:32 +1100

    Fixed zsh history saving

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -18,13 +18,14 @@ fi
 export BLOCKSIZE=1k # set default blocksize for ls, df, du
 
 # ---- HISTORY ----
-HISTFILE="ZSH_CACHE_DIR/.zsh_history"
+HISTFILE="$ZSH_CACHE_DIR/.zsh_history"
 HISTSIZE=99999
 HISTFILESIZE=999999
 SAVEHIST=$HISTSIZE
 setopt BANG_HIST                 # Treat the '!' character specially during expansion.
 #setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
-#setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt APPENDHISTORY             # 
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
 setopt SHARE_HISTORY             # Share history between all sessions.
 # setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
 # setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
