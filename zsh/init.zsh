74761b95a09688b889459fe32a44cd91b45825df
turnspike <hello@turnspike.com>
2022-02-16 13:06:50 +1100

    Cleanup script timer

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -5,7 +5,7 @@ source $ZCONF/colors.zsh # load color definitions for pretty output
 
 aw_title "ʕっ•ᴥ•ʔっ.... starting zsh "$ZSH_VERSION
 aw_msg ""
-aw_msg "loading config from $ZCONF"
+aw_msg "loading config from "$ZCONF
 
 source $ZCONF/plugins.zsh # load plugins
 source $ZCONF/environment.zsh # load environment settings
@@ -21,7 +21,6 @@ function { # localise vars
   local ZSTOP=`date +%s`
   local ZDIFF=$(($ZSTOP-$ZSTART))
   unset ZSTART # remove startup timer
-  aw_msg "startup took ${ZDIFF}ms."
+  aw_title "☯ zsh ready in ${ZDIFF}s."
 }
 
-aw_title "☯ zsh ready."
